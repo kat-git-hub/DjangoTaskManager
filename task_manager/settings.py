c881eb70db04d3931cdf0449562d6c0ccbfc5d22
H P <humanpizzza@gmail.com>
2022-02-14 16:01:30 +0300

    add badges

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#env_path = os.path.join(BASE_DIR, '.env')
 load_dotenv()
 
 
