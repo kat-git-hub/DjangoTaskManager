6bfec66e2796d9cb0d29718828f12247ac772b1a
H P <humanpizzza@gmail.com>
2023-05-10 22:22:42 +0200

    add tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -5,6 +5,7 @@ from statuses.models import Status
 from labels.models import Labels
 from tasks.models import Task
 
+
 class TaskTests(TestCase):
 
     def setUp(self):
@@ -30,13 +31,14 @@ class TaskTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.task.name)
-    
-    # def test_task_detail_view(self):
-    #     self.client.login(username='testuser1', password='testpass123')
-    #     url = reverse('tasks:tasks', kwargs={'pk': self.task.pk})
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, self.task.name)
+
+
+    def test_task_detail_view(self):
+        self.client.login(username='testuser1', password='testpass123')
+        url = reverse('tasks:view_task', kwargs={'pk': self.task.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.task.name)
 
     
     def test_task_create_view(self):
@@ -54,7 +56,8 @@ class TaskTests(TestCase):
         self.assertContains(response, 'Task created successfully.')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(Task.objects.filter(name='New Task').exists())
-    
+
+
     def test_task_update_view(self):
         self.client.login(username='testuser1', password='testpass123')
         url = reverse('tasks:update', args=[self.task.pk])
@@ -70,9 +73,8 @@ class TaskTests(TestCase):
         self.assertContains(response, 'Task updated.')
         self.task.refresh_from_db()
         self.assertEqual(self.task.name, 'Updated Task')
-        
 
-    
+
     def test_task_delete_view(self):
         self.client.login(username='testuser1', password='testpass123')
         url = reverse('tasks:delete', args=[self.task.pk])
---
+++ tests/test_users.py
@@ -0,0 +1,46 @@
+from django.urls import reverse
+from django.test import TestCase
+from users.models import User
+
+class TestUsers(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            first_name='Name1',
+            last_name='Last_name1',
+            username='testuser', 
+            password='testpass123')
+
+
+    def test_create_user(self):
+        response = self.client.post(reverse('users:create'), {
+            'username': 'newuser',
+            'password1': 'newpass123',
+            'password2': 'newpass123',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+
+
+    def test_update_user(self):
+        self.client.login(username='testuser', password='testpass123')
+        url = reverse('users:update', args=[self.user.id])
+        form_data = {
+            'username': 'testuser2',
+            'first_name':'Name_update',
+            'last_name': 'LastName_update',
+            'password1':'testpass123',
+            'password2':'testpass123',
+        }
+        response = self.client.post(url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('login'))
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'testuser2')
+
+
+    def test_delete_user(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('users:delete', args=[self.user.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('users:users'))
+        self.assertFalse(User.objects.filter(username=self.user).exists())
