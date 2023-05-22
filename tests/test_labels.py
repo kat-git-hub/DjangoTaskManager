9e9c2dd157b7d3fb25843144968c45dada73e492
H P <humanpizzza@gmail.com>
2023-05-22 11:22:59 +0200

    edit tests

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -18,9 +18,15 @@ class LabelsTest(TestCase):
         self.label.save()
 
     def test_label_creation(self):
-        label = Labels.objects.create(
-            name='Another Label')
-        self.assertEqual(label.name, 'Another Label')
+        self.client.login(username='testuser', password='testpass123')
+        url = reverse('labels:create')
+        data = {'name': 'Another Test Label'}
+        #label = Labels.objects.create(
+        #    name='Another Label')
+        response = self.client.post(url, data, follow=True)
+        self.assertContains(response, 'Label created successfully')
+        self.assertEqual(response.status_code, 200)
+        #self.assertEqual(label.name, 'Another Label')
     
     def test_labels_list(self):
         self.client.login(username='testuser', password='testpass123')
@@ -31,10 +37,14 @@ class LabelsTest(TestCase):
         self.assertEqual(str(self.label), 'Test Label')
 
     def test_update_label(self):
-        self.label.name = 'Updated Label'
-        self.label.save()
+        self.client.login(username='testuser', password='testpass123')
+        url = reverse('labels:update',  kwargs={'pk': 1})
+        data = {'name': 'Updated Test Label'}
+        response = self.client.post(url, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Label updated')
         updated_label = Labels.objects.get(pk=self.label.pk)
-        self.assertEqual(updated_label.name, 'Updated Label')
+        self.assertEqual(updated_label.name, 'Updated Test Label')
 
     def test_label_deletion(self):
         self.label.delete()
